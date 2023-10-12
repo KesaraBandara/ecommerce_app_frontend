3e1a9c14261dbae46dfd149293c60174b3b2a937
kesara <kesrabandara1998@gmail.com>
2023-10-12 23:06:43 +0530

    first

---
+++ src/views/Category/AddCategory.vue
@@ -0,0 +1,141 @@
+<template>
+    <div>Add Category</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3 class="pt-3">Add Category</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <form>
+                <div class="form-group">
+                    <label> Name</label>
+                    <input type="text" class="form-control" v-model="categoryName">
+                </div>
+                <div class="form-group">
+                    <label> Description</label>
+                    <textarea type="text" class="form-control" v-model="description"/>
+                </div>
+                <div class="form-group">
+                    <label> Image</label>
+                    <input type="text" class="form-control" v-model="imageUrl">
+            </div>
+            <button type="button" class="btn btn-submit" @click="addCategory">Submit</button>
+            </form>
+            </div>
+            <div class="col-3"></div>
+          
+        </div>
+    </div>
+</template>
+<!--<script>-->
+
+<!--const axios = require("axios");-->
+<!--const sweetalert = require("sweetalert");-->
+<!--//-->
+<!--// export default {-->
+<!--//     data(){-->
+<!--//         return{-->
+<!--//             categoryName:"",-->
+<!--//             description:"",-->
+<!--//             imageUrl:"",-->
+<!--//-->
+<!--//         }-->
+<!--//     },-->
+<!--//     methods:{-->
+<!--//         addCategory(){-->
+<!--//             console.log(this.categoryName, this.description);-->
+<!--//             const newCategory = {-->
+<!--//                 categoryName: this.categoryName,-->
+<!--//                 description: this.description,-->
+<!--//                 imageUrl: this.imageUrl,-->
+<!--//             };-->
+<!--//           const baseURL = "";-->
+<!--//           axios({-->
+<!--//             method: "post",-->
+<!--//             url: `${baseURL}/category/create`,-->
+<!--//             data: JSON.stringify(newCategory),-->
+<!--//             headers: {-->
+<!--//               "Content-Type": "application/json",-->
+<!--//             },-->
+<!--//           })-->
+<!--//               .then(() => {-->
+<!--//                 sweetalert({-->
+<!--//                   text: "Category added successfully",-->
+<!--//                   icon: "success",-->
+<!--//                 });-->
+<!--//               })-->
+<!--//               .catch((err) => {-->
+<!--//                 console.log(err);-->
+<!--//               });-->
+<!--//         },-->
+<!--//     },-->
+<!--// };-->
+<!--const baseURL = "http://localhost:8080"; // Update the base URL to your local API server-->
+<!--axios({-->
+<!--  method: "post",-->
+<!--  url: `${baseURL}/api/v1/category/create`, // Update the API endpoint as well-->
+<!--  data: JSON.stringify(newCategory),-->
+<!--  headers: {-->
+<!--    "Content-Type": "application/json",-->
+<!--  },-->
+<!--})-->
+<!--    .then(() => {-->
+<!--      sweetalert({-->
+<!--        text: "Category added successfully",-->
+<!--        icon: "success",-->
+<!--      });-->
+<!--    })-->
+<!--    .catch((err) => {-->
+<!--      console.log(err);-->
+<!--    });-->
+
+<!--</script>-->
+
+<script>
+const axios = require("axios");
+const sweetalert = require("sweetalert");
+
+export default {
+  data() {
+    return {
+      categoryName: "",
+      description: "",
+      imageUrl: "",
+      newCategory: {} // Define the newCategory object here
+    };
+  },
+  methods: {
+    addCategory() {
+      console.log(this.categoryName, this.description);
+      this.newCategory = { // Assign values to newCategory object
+        categoryName: this.categoryName,
+        description: this.description,
+        imageURL: this.imageUrl
+      };
+
+      const baseURL = "http://localhost:8083";
+      axios({
+        method: "post",
+        url: `${baseURL}/api/v1/category/create`,
+        data: JSON.stringify(this.newCategory), // Use this.newCategory
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+          .then(() => {
+            sweetalert({
+              text: "Category added successfully",
+              icon: "success"
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }
+  }
+};
+</script>
+<style scoped></style>
---
+++ src/views/Category/Category.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h3 class="pt-3"> Categories</h3>
+        <router-link :to="{name : 'AddCategory'}">
+          <button class="btn" style="float:right"> Add Category</button>
+        </router-link>
+      </div>
+    </div>
+    <div class="row">
+    <div v-for="category of categories" :key="category.id" class="col-xl-4 col-md-6 col-12 pt-3 d-flex">
+      <CategoryBox :category="category"></CategoryBox>
+    </div>
+    </div>
+  </div>
+</template>
+
+<!--<script>-->
+<!--import CategoryBox from '../../components/Category/CategoryBox.vue'-->
+<!--import axios from "axios";-->
+<!--// import CategoryBox from "@/components/Category/CategoryBox.vue";-->
+<!--export default {-->
+<!--  name: "CategoryView",-->
+<!--  components: {CategoryBox},-->
+<!--  data() {-->
+<!--    return {-->
+<!--      baseURL: "*+,-->
+<!--      categories: [],-->
+<!--    };-->
+<!--  },-->
+<!--  methods: {-->
+<!--    async getCategories() {-->
+<!--      await axios-->
+<!--          .get(`${this.baseURL}/category`)-->
+<!--          .then((res) => (this.categories = res.data))-->
+<!--          .catch((err) => console.log(err));-->
+<!--    },-->
+<!--  },-->
+<!--  mounted() {-->
+<!--    this.getCategories();-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
+<script>
+import CategoryBox from '../../components/Category/CategoryBox.vue'
+import axios from "axios";
+
+export default {
+  name: "CategoryView",
+  components: {CategoryBox},
+  data() {
+    return {
+      baseURL: "http://localhost:8083", // Change this to your local API server address
+      categories: [],
+    };
+  },
+  methods: {
+    async getCategories() {
+      await axios
+          .get(`${this.baseURL}/api/v1/category/`) // Make sure to use the correct API endpoint
+          .then((res) => (this.categories = res.data))
+          .catch((err) => console.log(err));
+    },
+  },
+  mounted() {
+    this.getCategories();
+  },
+};
+</script>
+
+
+<style scoped></style>
